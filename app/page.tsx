52aa1e89fd0a35f7037dec470348720aae3b3c6b
oStone <lws2269@kakao.com>
2026-01-15 22:18:45 +0900

    feat: fetch, error, loading 변수세팅

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,33 @@
 'use client';
 
-import { useEffect } from 'react';
+/* eslint-disable @next/next/no-img-element */
 
-import { fetchItems } from '@/lib/api/item';
+import { useEffect, useState } from 'react';
+
+import type { Item } from '@/lib/api/item';
+import { fetchItems, sortAndGroupItems } from '@/lib/api/item';
 
 const Page = () => {
+  const [items, setItems] = useState<Item[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     let cancelled = false;
 
     fetchItems()
       .then((data) => {
         if (cancelled) return;
-        console.log('items:', data);
+        const { activateItems, soldOutItems } = sortAndGroupItems(data);
+        const newItems = [...activateItems, ...soldOutItems];
+        setItems(newItems);
+        setIsLoading(false);
       })
       .catch((err) => {
+        if (cancelled) return;
         console.error(err);
+        setError(err instanceof Error ? err.message : '알 수 없는 오류가 발생했어요.');
+        setIsLoading(false);
       });
 
     return () => {
