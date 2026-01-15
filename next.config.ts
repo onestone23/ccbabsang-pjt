853affb845aecc9c86d9ea7c7ea671bf01eae969
oStone <lws2269@kakao.com>
2026-01-16 00:06:11 +0900

    chore: 배포설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/ccbabsang-pjt',
+  assetPrefix: '/ccbabsang-pjt',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
